008a7eb0dc09b885cf33647ab9b0da8197a93315
BLuk8712 <252317@edu.p.lodz.pl>
2023-11-19 14:59:16 +0100

    Nowa metoda policz

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+int Policz(int a, int b){
+    return a+b
+}
 int main() {
-    printf("Hello, World!\n");
+    int a,b;
+    printf("Podaj a: ");
+    scanf("%d",&a);
+    printf("Podaj b: ");
+    scanf("%d",&b);
+    printf("Wynik %d + %d = %d",a,b, Policz(a,b));
     return 0;
 }
