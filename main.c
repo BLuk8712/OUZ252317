799b1e8e275fa7020485f616da0d63e37a8785c1
BLuk8712 <252317@edu.p.lodz.pl>
2023-11-19 15:05:25 +0100

    Zmiana w metodzie policz na mnożenie

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-int Policz(int a, int b){
-    return a+b
+
+int Policz(int a, int b) {
+    return a * b
 }
+
 int main() {
-    int a,b;
+    int a, b;
     printf("Podaj a: ");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("Podaj b: ");
-    scanf("%d",&b);
-    printf("Wynik %d + %d = %d",a,b, Policz(a,b));
+    scanf("%d", &b);
+    printf("Wynik %d * %d = %d", a, b, Policz(a, b));
     return 0;
 }
