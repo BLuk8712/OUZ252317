fab8dde89cb988f131de01229d7b2078127de19a
BLuk8712 <252317@edu.p.lodz.pl>
2023-11-19 14:54:00 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OUZ252317.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OUZ252317.dir/main.c.o"
+  "OUZ252317"
+  "OUZ252317.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OUZ252317.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
